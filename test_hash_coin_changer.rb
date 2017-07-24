fd4615c136c649caa8fe5a5e3513f08773f9897e
jkeiffer1 <saros82@hotmail.com>
2017-07-23 21:55:14 -0400

    reworked code to handle multiple types of change, added tests to test off change

--- test_hash_coin_changer.rb
+++ test_hash_coin_changer.rb
@@ -18,7 +18,22 @@ class Coin_changer_test < Minitest::Test
 		assert_equal({quarter: 0, dime: 1, nickel: 0, penny: 0}, coin_changer(10))
 	end
 
+	def test_1_quarter
+		assert_equal({quarter: 1, dime: 0, nickel: 0, penny: 0},coin_changer(25))
+	end
 
+	def test_26_cents
+		assert_equal({quarter: 1, dime: 0, nickel: 0, penny: 1}, coin_changer(26))
+	end
+
+	def test_53_cents
+		assert_equal({quarter: 2, dime: 0, nickel: 0, penny: 3}, coin_changer(53))
+	end
+
+	def test_96_cents
+		assert_equal({quarter: 3, dime: 2, nickel: 0, penny: 1}, coin_changer(96))
+	end
+	
 
 
 end
