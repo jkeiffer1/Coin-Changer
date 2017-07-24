3b1a526f1d0649a7657a34f0a94b8e0b26ab83fb
jkeiffer1 <saros82@hotmail.com>
2017-07-23 21:33:26 -0400

    added dime value

---
+++ Hash_coin_changer.rb
@@ -0,0 +1,10 @@
+def coin_changer(change)
+	coins = {quarter: 25, dime: 10, nickel: 5, penny: 1}
+	if change  == 1
+		{quarter: 0, dime: 0, nickel: 0, penny: 1}
+	elsif change == 5
+		{quarter: 0, dime: 0, nickel: 1, penny: 0}
+	elsif change == 10
+		{quarter: 0, dime: 1, nickel: 0, penny: 0}
+	end
+end
--- test_hash_coin_changer.rb
+++ test_hash_coin_changer.rb
@@ -6,6 +6,18 @@ class Coin_changer_test < Minitest::Test
 		assert_equal(1,1)
 	end
 
+	def test_hash_for_1_cent
+		assert_equal({quarter: 0, dime: 0, nickel: 0, penny: 1}, coin_changer(1))
+	end
+
+	def test_hash_for_1_nickel
+		assert_equal({quarter: 0, dime: 0,nickel: 1, penny: 0},coin_changer(5))
+	end
+
+	def test_1_dime
+		assert_equal({quarter: 0, dime: 1, nickel: 0, penny: 0}, coin_changer(10))
+	end
+
 
 
 
